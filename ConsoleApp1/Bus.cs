f91eb2ee604c128b05c9e42b4e0fa95ebbe9be2c
Джабашев Нурбек Кадербекович <@corp.itstep>
2019-01-24 20:40:20 +0600

    AstraPlat

---
+++ ConsoleApp1/Bus.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AstraPlat
+{
+    class Bus
+    {
+        private string _cityBus;
+        public string CityBus
+        {
+            get
+            {
+                Console.WriteLine("Городской автобус");
+                return _cityBus;
+            }
+            set
+            {
+                _cityBus = value;
+            }
+        }
+        private string _suburbanBus;
+        public string SuburbanBus
+        {
+            get
+            {
+                Console.WriteLine("Загородний автобус");
+                return _suburbanBus;
+            }
+            set
+            {
+                _suburbanBus = value;
+            }
+        }
+
+        private string _expressBus;
+        public string ExpressBus
+        {
+            get
+            {
+                Console.WriteLine("Экспресс автобус");
+                return _expressBus;
+            }
+            set
+            {
+                _expressBus = value;
+            }
+        }
+    }
+}
